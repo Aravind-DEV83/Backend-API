c4c28e97ec1ca387d4c4fa436c8b06b34e1331cf
Aravind <aravind.jarpala@springml.com>
2024-03-01 16:01:02 +0530

    Committing all changes

--- Javascript/sample.js
+++ Javascript/sample.js
@@ -8,6 +8,14 @@ const product = {
 // let Name = prompt("Enter your Full Name")
 
 // let userName = "@" + Name + Name.length
+let arr = [250, 645, 300, 900, 50];
+let su = 0;
+const discount = 10;
+for (let i = 0; i<arr.length; i++) {
+    arr[i] = arr[i] - arr[i] / discount
+}
+console.log(arr);
+
 
 let jsonData = JSON.stringify(product)
 let jsobject = JSON.parse(jsonData)
